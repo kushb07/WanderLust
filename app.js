ba039e7f43e82a366e14a1ee039a6350bd5d2004
Kush Bajaj <129482852+kushb07@users.noreply.github.com>
2024-01-06 17:59:51 +0530

    Update app.js env variable

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(express.static(path.join(__dirname , "/public")));
 
 
 const store = MongoStore.create({
-    mongoUrl : 'mongodb+srv://Kush:Kush0705@cluster0.fdyqvum.mongodb.net/?retryWrites=true&w=majority',
+    mongoUrl :  process.env.ATLASDB_URL,
     crypto:{
         secret : process.env.SECRET,
     },
